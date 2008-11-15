f9fd23fbec91582bfa40d3f7d9a57cef1444c8c6
f1racer328 <f1racer328>
2008-11-15 20:15:34 +0000

    Added build

---
+++ build/iPaint.build/Release/iPaint.build/DerivedSources/iPaint_vers.c
@@ -0,0 +1,1 @@
+ const unsigned char iPaintVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:iPaint  PROJECT:iPaint-1.1" "\n"; const double iPaintVersionNumber __attribute__ ((used)) = (double)1.1;
